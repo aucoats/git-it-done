811f1260288e3482e8879c5abb239c292bc1c46e
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-11-10 12:22:22 -0500

    this should have been in feature/languages. language buttons added and functional

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -35,7 +36,7 @@ var getUserRepos = function(user) {
             // Notice this .catch() is chained onto end of .then() method
             alert("Unable to connect to GitHub");
         });
-  };
+};
 
   var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
@@ -82,6 +83,33 @@ var getUserRepos = function(user) {
         repoContainerEl.appendChild(repoEl);
     }
     
-  }
+  };
+
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+  };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
